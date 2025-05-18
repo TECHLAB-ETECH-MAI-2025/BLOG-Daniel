00ec19a8e0d6949ebbbffc51c550b645df9c9ab3
Sun-2302 <hei.dera.932@gmail.com>
2025-05-18 09:35:08 +0300

    refactor: upgrade CommentForm

--- src/Form/CommentForm.php
+++ src/Form/CommentForm.php
@@ -6,6 +6,8 @@ use App\Entity\Article;
 use App\Entity\Comment;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +16,23 @@ class CommentForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('author')
-            ->add('content')
+            ->add('author', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Votre nom',
+                    'required' => true
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Votre commentaire',
+                'attr' => [
+                    'class' => 'form-control',
+                    'rows' => 4,
+                    'placeholder' => 'Écrivez votre commentaire ici...',
+                    'required' => true
+                ]
+            ])
             ->add('createdAt')
             ->add('article', EntityType::class, [
                 'class' => Article::class,
